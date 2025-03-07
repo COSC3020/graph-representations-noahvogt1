31367848366f6d0c17660038c80830436e0d5a65
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-03-06 20:11:14 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,13 +2,13 @@ function convertToAdjList(adjMatrix) {
     let adjList = [];
 
     for (let i = 0; i < adjMatrix.length; i++) {
-    adjList[i] = [];
-    for (let j = 0; j < adjMatrix[i].length; j++) {
-      if (adjMatrix[i][j] === 1) {
-        adjList[i].push(j);
-      }
+        adjList[i] = [];
+        for (let j = 0; j < adjMatrix[i].length; j++) {
+            if (adjMatrix[i][j] === 1) {
+                adjList[i].push(j);
+            }
+        }
     }
-  }
 
-  return adjList;
+    return adjList;
 }
